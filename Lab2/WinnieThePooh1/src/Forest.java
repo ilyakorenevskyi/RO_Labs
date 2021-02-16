d8f83aa71c4f2ae02f2f933b32d60c185c990499
Илья Кореневский <ilyakorenevskii@gmail.com>
2021-02-16 17:56:31 +0200

    fix

---
+++ Lab2/WinnieThePooh1/src/Forest.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class Forest {
+    private int size = 10;
+    private boolean[][] forest;
+    public Forest(int size){
+        this.size = size;
+        for(int i = 0; i<size; i++)
+            for(int j = 0; j<size; j++)
+                forest[i][j] = false;
+        Random rand = new Random();
+        forest[rand.nextInt(size)][rand.nextInt(size)] = true;
+    }
+}
--- Lab2/WinnieThePooh1/src/Main.java
+++ Lab2/WinnieThePooh1/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Forest forest = new Forest(20);
+    }
+
 }
